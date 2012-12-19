9e18c95062604f724abc8385a1f14850ae1c6035
Bret Little <blittle@simplifile.com>
2012-12-19 12:26:52 -0700

    Add support for Backbone v0.9.9

--- backbone-nested.js
+++ backbone-nested.js
@@ -1,19 +1,15 @@
 /**
  * Backbone Nested Models
  * Author: Bret Little
- * Version: 0.2
+ * Version: 0.3
  *
  * Nested model support in Backbone.js
  *
  **/
 
 (function(Backbone) {
-    var set = Backbone.Model.prototype.set,
-        add = Backbone.Collection.prototype.add,
-        Model = Backbone.Model,
-        Collection = Backbone.Collection,
-        parent;
-
+    var Model = Backbone.Model,
+        Collection = Backbone.Collection;
 
     Backbone.Model.prototype.setRelation = function(attr, val) {
         var relation = this.attributes[attr],
@@ -89,23 +85,21 @@
         return val;
     };
 
-    Backbone.Model.prototype.set = function(key, value, options) {
-        var attrs, attr, val;
+    Backbone.Model.prototype.set = function(key, val, options) {
+        var attr, attrs;
+        if (key == null) return this;
 
         // Handle both `"key", value` and `{key: value}` -style arguments.
-        if (_.isObject(key) || key == null) {
+        if (_.isObject(key)) {
             attrs = key;
-            options = value;
+            options = val;
         } else {
-            attrs = {};
-            attrs[key] = value;
+            (attrs = {})[key] = val;
         }
 
         // Extract attributes and options.
-        options || (options = {});
-        if (!attrs) return this;
-        if (attrs instanceof Model) attrs = attrs.attributes;
-        if (options.unset) for (attr in attrs) attrs[attr] = void 0;
+        var silent = options && options.silent;
+        var unset = options && options.unset;
 
         // Run validation.
         if (!this._validate(attrs, options)) return false;
@@ -113,10 +107,7 @@
         // Check for changes of `id`.
         if (this.idAttribute in attrs) this.id = attrs[this.idAttribute];
 
-        var changes = options.changes = {};
         var now = this.attributes;
-        var escaped = this._escapedAttributes;
-        var prev = this._previousAttributes || {};
 
         // For each `set` attribute...
         for (attr in attrs) {
@@ -124,28 +115,17 @@
 
             val = this.setRelation(attr, val);
 
-            // If the new and current value differ, record the change.
-            if (!_.isEqual(now[attr], val) || (options.unset && _.has(now, attr))) {
-                delete escaped[attr];
-                (options.silent ? this._silent : changes)[attr] = true;
-            }
-
-            // Update or delete the current value.
-            options.unset ? delete now[attr] : now[attr] = val;
-
-            // If the new and previous value differ, record the change.  If not,
-            // then remove changes for this attribute.
-            if (!_.isEqual(prev[attr], val) || (_.has(now, attr) != _.has(prev, attr))) {
-                this.changed[attr] = val;
-                if (!options.silent) this._pending[attr] = true;
-            } else {
-                delete this.changed[attr];
-                delete this._pending[attr];
-            }
+            // Update or delete the current value, and track the change.
+            unset ? delete now[attr] : now[attr] = val;
+            this._changes.push(attr, val);
         }
 
+        // Signal that the model's state has potentially changed, and we need
+        // to recompute the actual changes.
+        this._hasComputed = false;
+
         // Fire the `"change"` events.
-        if (!options.silent) this.change(options);
+        if (!silent) this.change(options);
         return this;
     };
 
