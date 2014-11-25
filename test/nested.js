561cf176df476c1f1e51987f56cde1f83471bd84
Bret Little <bret.little@gmail.com>
2014-11-25 10:27:53 -0700

    Add test

---
+++ test/nested.js
@@ -0,0 +1,40 @@
+ddescribe("Nested relations", function() {
+	var item, Item;
+
+	beforeEach(function() {
+    var Outlook = Backbone.Model.extend({
+      relations: {
+        "scenarios": Backbone.Collection
+      }
+    });
+
+    var OutlookCollection = Backbone.Collection.extend({
+      model: Outlook
+    });
+
+    Item = Backbone.Model.extend({
+      relations: {
+        "outlooks": OutlookCollection
+      }
+    });
+
+    item = new Item();
+    item.set({
+      outlooks: [
+        {
+          'name': "Bret",
+          "scenarios": [
+            {
+              "someValue": true
+            }
+          ]
+        }
+      ]
+    });
+	});
+
+	it("Should build a nested relation", function() {
+    expect(item.get('outlooks').at(0).get('scenarios').at(0).get('someValue')).toBe(true);
+	});
+
+});
