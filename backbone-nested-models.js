f7b47b8f51f5b0309c01a25644eb279211f4681d
Collin Donahue-Oponski <collindo@gmail.com>
2015-07-01 17:58:26 -0600

    Use built-in Backbone.Collection.prototype.set
    ... which already performs intelligent adds/updates/removes.

--- backbone-nested-models.js
+++ backbone-nested-models.js
@@ -43,38 +43,8 @@
                 // within the collection.
                 if(val instanceof Collection || val instanceof Array){
                     val = val.models || val;
-                    modelsToAdd = _.clone(val);
 
-                    relation.each(function(model, i) {
-
-                        // If the model does not have an "id" skip logic to detect if it already
-                        // exists and simply add it to the collection
-                        if(typeof model[id] == 'undefined') return;
-
-                        // If the incoming model also exists within the existing collection,
-                        // call set on that model. If it doesn't exist in the incoming array,
-                        // then add it to a list that will be removed.
-                        var rModel = _.find(val, function(_model) {
-                            return _model[id] === model[id];
-                        });
-
-                        if(rModel) {
-                            model.set(rModel.toJSON ? rModel.toJSON() : rModel);
-
-                            // Remove the model from the incoming list because all remaining models
-                            // will be added to the relation
-                            modelsToAdd.splice(i,1);
-                        } else {
-                            modelsToRemove.push(model);
-                        }
-
-                    });
-
-                    _.each(modelsToRemove, function(model) {
-                        relation.remove(model);
-                    });
-
-                    relation.add(modelsToAdd);
+                    relation.set(val);
 
                 } else {
 
