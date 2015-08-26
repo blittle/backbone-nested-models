2649dbd125bd14dfdbac43ce262fc37d9c5e19cc
Bret Little <bret.little@gmail.com>
2015-08-25 21:30:24 -0600

    Add headless tests for TravisCI

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,10 @@ module.exports = function(grunt) {
     },
     karma: {
       unit: {
-        configFile: 'karma.conf.js'
+				configFile: 'karma.conf.js'
+			},
+      headless: {
+				configFile: 'karma-headless.conf.js'
       }
     }
   });
